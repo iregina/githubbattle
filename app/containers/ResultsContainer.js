ded3e39cc6a25fcd1ed7c6fd55c5771340980abb
iregina <reginawong14@gmail.com>
2016-04-15 16:53:40 -0700

    add state to results container

--- app/containers/ResultsContainer.js
+++ app/containers/ResultsContainer.js
@@ -1,12 +1,23 @@
 var React = require('react');
 var Results = require('../components/Results')
+var githubHelpers = require('../utils/githubHelpers');
 
 var ResultsContainer = React.createClass({
+	getInitialState: function(){
+		return {
+			isLoading: true,
+			scores: []
+		}
+	},
+	componentDidMount: function(){
+		console.log(this.props.location.state.playersInfo)
+	},
 	render: function() {
 		return (
-			<Results />
+			<Results isLoading={this.state.isLoading} scores={this.state.scores} />
 			);
 	}
+	
 });
 
 module.exports = ResultsContainer;
